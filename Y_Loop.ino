397db9410aa60166ec5a50b9d34a67b7e9afd620
robertbrianhemingway <jafger@gmail.com>
2020-05-04 14:26:49 +1000

    Add files via upload

---
+++ Y_Loop.ino
@@ -0,0 +1,31 @@
+void loop() {
+    Blynk.run();
+
+		// flash D3 to signal loop is running
+		flashD3();
+    // first check for manualOnOffButton getPirPinState
+    if (manualOnOffButton) {
+        // manually force LEDs on
+        setSirenOn();
+        Front.setOn();
+        Back.setOn();
+    } else {
+        // poll the pirPins
+        if (Front.getPirPinState()) Front_ISR();
+        if (Back.getPirPinState()) Back_ISR();
+
+        // check if leds on, and if so is time up
+        bool front = Front.isPirTimeUp();
+        bool back = Back.isPirTimeUp();
+        if (!(front || back)) setSirenOff();
+        if (lastSirenValue != sirenValue) {
+          // change since last check
+          Blynk.virtualWrite(V0,sirenValue);
+          lastSirenValue = sirenValue;
+          if (sirenValue > 200) {
+             if (eMailButtonState) Blynk.email("Subject: Outside LEDs","Outside PIRs activated");
+             terminalWrite(" Outside LEDs ON");
+          }
+        }
+    }
+}
